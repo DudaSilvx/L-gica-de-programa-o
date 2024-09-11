4fcd81e19a2f153a41b8b2c24675af002d53dbf8
DudaSilvx <113145263+DudaSilvx@users.noreply.github.com>
2024-09-10 21:15:52 -0300

    Arquivos JS - 1

--- Aula 1/ex01.js
+++ Aula 1/ex01.js
@@ -1,4 +1,4 @@
-var nome1 = "João"; /* toda vez que definir algo tem que escrever (var, let ou const) */
+var nome1 = "Breno"; /* toda vez que definir algo tem que escrever (var, let ou const) */
 var nome2 = "Maria";
 
 console.log(nome1)
