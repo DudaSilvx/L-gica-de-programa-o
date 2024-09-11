2cbae85d0ff94f22069f2131bf633d8eaad45dd9
DudaSilvx <113145263+DudaSilvx@users.noreply.github.com>
2024-09-10 21:21:26 -0300

    Arquivos JS - 3

--- Aula 3/parseint.js
+++ Aula 3/parseint.js
@@ -1,3 +1,3 @@
 /* parseInt - conversao de string para int, parseFloat - converte string para float */
-let numeroInteiro = parseInt("3.5"); 
+let numeroInteiro = parseInt("3.5"); /* parseInt - transforma o que esta no prompt em número inteiro */
 console.log(numeroInteiro);
