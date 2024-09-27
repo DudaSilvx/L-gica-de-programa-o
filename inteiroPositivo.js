36dc4494d797fe354186c465bb2933e001086bc5
DudaSilvx <113145263+DudaSilvx@users.noreply.github.com>
2024-09-26 21:24:28 -0300

    Numero inteiro positivo

---
+++ inteiroPositivo.js
@@ -0,0 +1,12 @@
+/* Elabore um programa que solicite ao usuário um número inteiro positivo
+e exiba uma contagem regressiva de 10 até esse número (em decrementos de 1).
+Caso o valor digitado seja menor que 10, exiba uma mensagem de erro. */
+
+let numero = parseInt(prompt("Digite um número"))
+
+let regressiva = 10;
+
+while (regressiva > numero){
+    console.log(regressiva--)
+}
+console.log(numero)
